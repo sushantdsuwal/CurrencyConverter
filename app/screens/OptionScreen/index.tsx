4772b006618694a6c2683f85aaa82c6d9e320038
Sushant Suwal <sushantdsuwal@gmail.com>
2021-10-01 14:32:16 +0545

    vector icon add

--- app/screens/OptionScreen/index.tsx
+++ app/screens/OptionScreen/index.tsx
@@ -1,12 +1,16 @@
 import { useNavigation } from '@react-navigation/core';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import PropTypes from 'prop-types';
 
-import React, { Component } from 'react';
-import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
+import React from 'react';
+import { ScrollView, StatusBar, Platform } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { ListItem, Separator } from '~/components/common/List';
 import { RootStackParamList } from '~/navigator';
+
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
+const ICON_COLOR = '#868686';
+const ICON_SIZE = 23;
 
 export type HomeScreenNavigationProps = NativeStackNavigationProp<
   RootStackParamList,
@@ -21,9 +25,13 @@ export default function OptionScreen(): JSX.Element {
       <ListItem
         text="Themes"
         onPress={() => navigation.navigate('Theme')}
-        // customIcon={
-        //   <Ionicons name={`${ICON_PREFIX}-arrow-forward`} size={ICON_SIZE} color={ICON_COLOR} />
-        // }
+        customIcon={
+          <Ionicons
+            name={`${ICON_PREFIX}-arrow-forward`}
+            size={ICON_SIZE}
+            color={ICON_COLOR}
+          />
+        }
       />
       <Separator />
     </ScrollView>
